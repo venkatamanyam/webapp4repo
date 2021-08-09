834a9474493c744ab483624917f9fc7d7843e776
venkatamanyam <kvsubbu.db2@gmail.com>
2021-08-09 14:08:28 +0530

    how

---
+++ src/main/java/com/example/SBApplication/Sbwebapp2Application.java
@@ -0,0 +1,13 @@
+package com.example.SBApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbwebapp2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbwebapp2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SBApplication/Sbwebapp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SBApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Sbwebapp2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
